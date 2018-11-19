d5c4ba4704071bdd62526690715f47d210bd9f22
Felix <boyboby@163.com>
2018-11-19 18:31:48 +0800

    Initial Commit

---
+++ Pray/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pray
+//
+//  Created by 潘磊 on 2018/11/19.
+//  Copyright © 2018 PL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
