90a3c6894532c2e1acaf18169683755f9e6fa814
Felix <boyboby@163.com>
2018-11-23 10:09:58 +0800

    judge the size of the image

--- Pray/AppDelegate.swift
+++ Pray/AppDelegate.swift
@@ -16,8 +16,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.rootViewController = createTabBarController()
+//        window?.makeKeyAndVisible()
+          return true
     }
+    
+//    private func createTabBarController() -> UITabBarController {
+//        let tabBarController = FluidTabBarController()
+//        tabBarController.tabBar.tintColor = UIColor(red: 0.2431372549, green: 0.4235294118, blue: 1, alpha: 1)
+//        let viewControllers = [
+//            ("News", #imageLiteral(resourceName: "news")),
+//            ("Requests", #imageLiteral(resourceName: "requests")),
+//            ("Events", #imageLiteral(resourceName: "events")),
+//            ("Members", #imageLiteral(resourceName: "members")),
+//            ("Account", #imageLiteral(resourceName: "profile"))
+//            ].map(createSampleViewController)
+//        tabBarController.setViewControllers(viewControllers, animated: true)
+//        return tabBarController
+//    }
+//
+//    private func createSampleViewController(title: String, icon: UIImage) -> UIViewController {
+//        let viewController = UIViewController()
+//        viewController.view.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9529411765, blue: 0.968627451, alpha: 1)
+//        let item = FluidTabBarItem(title: title, image: icon, tag: 0)
+//        item.imageColor = #colorLiteral(red: 0.7960784314, green: 0.8078431373, blue: 0.8588235294, alpha: 1)
+//        viewController.tabBarItem = item
+//        return viewController
+//    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
